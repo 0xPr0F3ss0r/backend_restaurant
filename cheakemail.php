a845695301bfb0a9460cea1b4c20bb321e63689a
Kebir Hani <145288127+0xPr0F3ss0r@users.noreply.github.com>
2025-08-19 14:21:02 -1100

    Update cheakemail.php

--- cheakemail.php
+++ cheakemail.php
@@ -8,13 +8,12 @@ $VerifyCode = rand(10000,99999);
 $stmt = $con->prepare("SELECT * FROM users WHERE user_email = ?");
 $stmt->execute(array($email));
 $count = $stmt->rowCount();
-//result($count ,$msg = '',$error = 'email not found');
+
 
 if($count > 0 )  {
     $stmt = $con->prepare("UPDATE users SET user_verifycode='$VerifyCode' Where user_email = ?");
     $stmt->execute(array($email));
     $count = $stmt->rowCount();
     result($count ,$msg='send mail to verify' ,$error = 'email not found');
-    //sendMail($email,'verify code Ecommerce',"Verify Code $VerifyCode");
 
 }
